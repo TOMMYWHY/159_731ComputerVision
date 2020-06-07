f9c367fe786ea4ccf50f3912481ac1f48ac63391
tommy <tommywhy1989@gmail.com>
2020-06-08 10:39:20 +1200

    project04:modeling test done

--- assignment3/demo/src/demo.cpp
+++ assignment3/demo/src/demo.cpp
@@ -31,6 +31,8 @@
 
 using namespace std;
 using namespace cv;
+using namespace cv::ml;
+
 
 #define Mpixel(image,x,y) ( (uchar *) ( ((image).data) + (y)*((image).step) ) ) [(x)]
 #define MpixelB(image,x,y) ( (uchar *) ( ((image).data) + (y)*((image).step) ) ) [(x)*((image).channels())]
@@ -40,106 +42,276 @@ using namespace cv;
 
 
 #if 001
+static bool read_num_class_data( const string& filename, int var_count, Mat* _data, Mat* _responses )
+{
+    const int M = 1024;
+    char buf[M+2];
 
-vector<double> EllipticFourierDescriptors(vector<Point>& contour , vector< float> CE) {
-    vector<float> ax, ay, bx, by;
-    vector<double> feature_values;
-    int m = contour.size();
-    cout <<"contour.size:" << m <<endl;
-    int n = 20;//number of CEs we are interested in computing
-     float t=(2*M_PI)/m;
-    for (int k = 0; k < n; k++) {
-        ax.push_back(0.0);
-        ay.push_back(0.0);
-        bx.push_back(0.0);
-        by.push_back(0.0);
-        for (int i = 0; i < m; i++) {
-            ax[k] = ax[k] + contour[i].x * cos((k + 1) * t * (i));
-            bx[k] = bx[k] + contour[i].x * sin((k + 1) * t * (i));
-            ay[k] = ay[k] + contour[i].y * cos((k + 1) * t * (i));
-            by[k] = by[k] + contour[i].y * sin((k + 1) * t * (i));
-        }
-        ax[k] = (ax[k]) / m;
-        bx[k] = (bx[k]) / m;
-        ay[k] = (ay[k]) / m;
-        by[k] = (by[k]) / m;
-    }
-    for (int k = 0; k < n; k++) {
-        CE.push_back(sqrt((ax[k] * ax[k] + ay[k] * ay[k]) / (ax[0] * ax[0] + ay[0] * ay[0])) +
-             sqrt((bx[k] * bx[k] + by[k] * by[k]) / (bx[0] * bx[0] + by[0] * by[0])));
-    }
-    for (int count = 0; count < n && count < CE.size(); count++) {
-        printf("%d CE %f ax %f ay %f bx %f by%f \n" ,count, CE[count], ax[count], ay[count], bx[count], by[count] );
-//        cout <<"CE index:"<<count <<"; CE value:"<< CE[count] <<endl;
-        Feature feature;
-//        feature.index = count;
-//        feature.value = CE[count];
-//        feature_list.push_back(feature);
-        feature_values.push_back(CE[count]);
+    Mat el_ptr(1, var_count, CV_32F);
+    int i;
+    vector<int> responses;
 
+    _data->release();
+    _responses->release();
+
+    FILE* f = fopen( filename.c_str(), "rt" );
+    if( !f )
+    {
+        cout << "Could not read the database " << filename << endl;
+        return false;
     }
 
-    return feature_values;
-
-}
-
-Mat get_obj_img(Mat org_img){
-    Mat gray_img, binary_img,obj_img;
-    blur(org_img,org_img, Size(3,3));
-    cvtColor ( org_img , gray_img , COLOR_BGR2GRAY ) ;
-    threshold ( gray_img , binary_img , 225 , 255 , THRESH_BINARY ) ;//todo 区域划分不精准
-    //    imshow("Binary",binary_img);
-
-    Mat reverse_img = 255 - binary_img;
-    org_img.copyTo(obj_img);
-    for (int x = 0; x < obj_img.cols; x++) {
-        for (int y = 0; y < obj_img.rows; y++) {
-            if(int(Mpixel(reverse_img ,x,y))==0){
-                MpixelB(obj_img ,x,y)= 0;
-                MpixelG(obj_img ,x,y)= 0;
-                MpixelR(obj_img ,x,y)= 0;
-            }
-        }
-    }
-//    imshow("obj_img",obj_img);
-    return obj_img;
-}
-
-vector<double> get_feature(Mat obj_img){
-    Mat obj_gray_img,obj_binary_img;
-    cvtColor( obj_img , obj_gray_img , COLOR_BGR2GRAY );
-    threshold ( obj_gray_img , obj_binary_img , 5 , 255 , THRESH_BINARY ) ;
-    vector<vector<Point> > contours ;
-    findContours ( obj_binary_img , contours ,RETR_EXTERNAL, CHAIN_APPROX_SIMPLE) ;
-    Mat drawing = Mat::zeros( obj_binary_img.size(), CV_8UC3 );
-    Scalar color = CV_RGB( 0, 255,0 );
-    int largestcontour=0;
-    long int largestsize=0;
-    vector< vector<Point> > filterContours;	// 筛选后的轮廓
-    vector< Point > hull;	// 凸包络的点集
-
-    for(int i = 0; i< contours.size(); i++ ) {
-
-        if (fabs(contourArea(Mat(contours[i]))) > 30000)	//判断手进入区域的阈值
+    for(;;)
+    {
+        char* ptr;
+        if( !fgets( buf, M, f ) || !strchr( buf, ',' ) )
+            break;
+        responses.push_back(buf[0]);
+//char test;
+//test=buf[0]+65;
+//responses.push_back(test);
+//        cout << "responses " << buf[0] << " " ;;//<<  endl;
+        ptr = buf+2;
+        for( i = 0; i < var_count; i++ )
         {
-            filterContours.push_back(contours[i]);
+            int n = 0;
+            sscanf( ptr, "%f%n", &el_ptr.at<float>(i), &n );
+            ptr += n + 1;
         }
-
+//        cout << el_ptr << endl;
+        if( i < var_count )
+            break;
+        _data->push_back(el_ptr);
     }
-    drawContours(drawing, filterContours, -1, Scalar(0,0,255), 1);
-    imshow("drawing" , drawing ) ;
-    vector<float> CE;
-    vector<double> feature_values = EllipticFourierDescriptors(contours[largestcontour], CE);
-    return feature_values;
+    fclose(f);
+    Mat(responses).copyTo(*_responses);
+
+    cout << "The database " << filename << " is loaded.\n";
+
+    return true;
 }
 
-int main (){
+template<typename T>
+static Ptr<T> load_classifier(const string& filename_to_load)
+{
+    // load classifier from the specified file
+    Ptr<T> model = StatModel::load<T>( filename_to_load );
+    if( model.empty() )
+        cout << "Could not read the classifier " << filename_to_load << endl;
+    else
+        cout << "The classifier " << filename_to_load << " is loaded.\n";
+
+    return model;
+}
+
+inline TermCriteria TC(int iters, double eps)
+{
+    return TermCriteria(TermCriteria::MAX_ITER + (eps > 0 ? TermCriteria::EPS : 0), iters, eps);
+}
+
+static void test_and_save_classifier(const Ptr<StatModel>& model,
+                                     const Mat& data, const Mat& responses,
+                                     int ntrain_samples, int rdelta,
+                                     const string& filename_to_save)
+{
+    int i, nsamples_all = data.rows;
+    double train_hr = 0, test_hr = 0;
+    int training_correct_predict=0;
+    // compute prediction error on training data
+    for( i = 0; i < nsamples_all; i++ )
+    {
+        Mat sample = data.row(i);
+        cout << "Sample: " << responses.at<int>(i)-48 << " row " << data.row(i) << endl;
+        float r = model->predict( sample );
+        cout << "Predict:  r = " << r << endl;
+        if( (int)r == (int)(responses.at<int>(i)-48) ) //prediction is correct
+            training_correct_predict++;
+
+        //r = std::abs(r + rdelta - responses.at<int>(i)) <= FLT_EPSILON ? 1.f : 0.f;
+
+
+        //if( i < ntrain_samples )
+        //    train_hr += r;
+        //else
+        //    test_hr += r;
+    }
+
+    //test_hr /= nsamples_all - ntrain_samples;
+    //train_hr = ntrain_samples > 0 ? train_hr/ntrain_samples : 1.;
+    printf("ntrain_samples %d training_correct_predict %d \n",ntrain_samples, training_correct_predict);
+    if( filename_to_save.empty() )  printf( "\nTest Recognition rate: training set = %.1f%% \n\n", training_correct_predict*100.0/ntrain_samples);
+
+
+    if( !filename_to_save.empty() )
+    {
+        model->save( filename_to_save );
+    }
+/*************   Example of how to predict a single sample ************************/
+// Use that for the assignment3, for every frame after computing the features, r is the prediction given the features listed in this format
+    //Mat sample = data.row(i);
+    Mat sample1 = (Mat_<float>(1,9) << 1.52101, 13.64, 4.4899998, 1.1, 71.779999, 0.059999999, 8.75, 0, 0);// 1
+    float r = model->predict( sample1 );
+    cout << "Prediction: " << r << endl;
+    sample1 = (Mat_<float>(1,9) << 1.518, 13.71, 3.9300001, 1.54, 71.809998, 0.54000002, 8.21, 0, 0.15000001);//2
+    r = model->predict( sample1 );
+    cout << "Prediction: " << r << endl;
+    sample1 = (Mat_<float>(1,9) << 1.51694,12.86,3.58,1.31,72.61,0.61,8.79,0,0);//3
+    r = model->predict( sample1 );
+    cout << "Prediction: " << r << endl;
+//    sample1 = (Mat_<float>(1,9) << );//4
+//    r = model->predict( sample1 );
+//    cout << "Prediction: " << r << endl;
+    sample1 = (Mat_<float>(1,9) << 1.5151401, 14.01, 2.6800001, 3.5, 69.889999, 1.6799999, 5.8699999, 2.2, 0);//5
+    r = model->predict( sample1 );
+    cout << "Prediction: " << r << endl;
+    sample1 = (Mat_<float>(1,9) << 1.51852, 14.09, 2.1900001, 1.66, 72.669998, 0, 9.3199997, 0, 0);//6
+    r = model->predict( sample1 );
+    cout << "Prediction: " << r << endl;
+    sample1 = (Mat_<float>(1,9) << 1.51131,13.69,3.2,1.81,72.81,1.76,5.43,1.19,0);//7
+    r = model->predict( sample1 );
+    cout << "Prediction: " << r << endl;
+
+/**********************************************************************/
+
+}
+
+static bool build_mlp_classifier( const string& data_filename,
+                      const string& filename_to_save,
+                      const string& filename_to_load ){
+    const int class_count = 7;//CLASSES //todo
+    Mat data;
+    Mat responses;
+    bool ok = read_num_class_data( data_filename, 9, &data, &responses );//third parameter: FEATURES
+
+    if( !ok ){
+        return ok;
+    }
+
+//    cout << "data= " << endl << " " << data << endl << endl;
+
+    Ptr<ANN_MLP> model;
+    int nsamples_all = data.rows;
+    int ntrain_samples = (int)(nsamples_all*1.0);//SPLIT
+    cout << ntrain_samples<<endl;
+    // Create or load MLP classifier
+    if( !filename_to_load.empty() )
+    {
+        // load MLP model
+        model = load_classifier<ANN_MLP>(filename_to_load);
+        if( model.empty() )
+            return false;
+        //ntrain_samples = 0;
+    }
+    else{
+        //create MLP model
+
+        Mat train_data = data.rowRange(0, ntrain_samples);
+        Mat train_responses = Mat::zeros( ntrain_samples, class_count, CV_32F );
+//        cout << "train_data= " << endl << " " << train_data << endl << endl;
+
+        // 1. unroll the responses
+        cout << "Unrolling the responses...\n";
+        for( int i = 0; i < ntrain_samples; i++ )
+        {
+            int cls_label = responses.at<int>(i) - 48;// - 'A'; //change to numerical classes, still they read as chars
+//            cout << "labels " << cls_label << endl;
+            train_responses.at<float>(i, cls_label) = 1.f;
+        }
+
+        // 2. train classifier
+        int layer_sz[] = { data.cols, 100, 100, class_count };
+//        int layer_sz[] = { data.cols, 128, 256, 128, 64, class_count };
+        cout <<  " sizeof layer_sz " << sizeof(layer_sz) << " sizeof layer_sz[0]) " << sizeof(layer_sz[0]) << endl;
+        int nlayers = (int)(sizeof(layer_sz)/sizeof(layer_sz[0]));
+        cout << " nlayers  " << nlayers << endl;
+        Mat layer_sizes( 1, nlayers, CV_32S, layer_sz );
+
+#if 1
+        int method = ANN_MLP::BACKPROP;
+        double method_param = 0.001;
+//        int max_iter = 300;
+        int max_iter = 100;
+#else
+        int method = ANN_MLP::RPROP;
+        double method_param = 0.1;
+        int max_iter = 1000;
+#endif
+
+        Ptr<TrainData> tdata = TrainData::create(train_data, ROW_SAMPLE, train_responses);
+
+        cout << "Training the classifier (may take a few minutes)...\n";
+        model = ANN_MLP::create();
+        model->setLayerSizes(layer_sizes);
+        model->setActivationFunction(ANN_MLP::SIGMOID_SYM, 0, 0);//sigmod
+        model->setTermCriteria(TC(max_iter,0));
+        model->setTrainMethod(method, method_param);
+        model->train(tdata);
+        cout << endl;
+    }
+    test_and_save_classifier(model, data, responses, ntrain_samples, 0, filename_to_save);
+    return true;
+}
+
+
+int main (int argc, char *argv[]){
+
 
 //    LoadData test("./images/test/","./res/test.data");
-    LoadData all_img("./images/all_img/","./res/all_img.data");
+//    LoadData all_img("./images/all_img/","./res/all_img.data");
 
     cout <<"write done" << endl;
-    waitKey(0);
+
+    /*build model*/
+    string data_filename = "./res/Glass.data";
+//    const string& data_filename = "./res/all_img.data";
+
+    string filename_to_save = "./res/Glass_ann_model.xml";
+
+    string filename_to_load = "";
+    for( int i = 1; i < argc; i++ )
+    {
+        if( strcmp(argv[i],"-data") == 0 ) // flag "-data letter_recognition.xml"
+        {
+            i++;
+            data_filename = argv[i];
+        }
+        else if( strcmp(argv[i],"-save") == 0 ) // flag "-save filename.xml"
+        {
+            i++;
+            filename_to_save = argv[i];
+            cout << "filename to save is "<< filename_to_save << endl;
+        }
+        else if( strcmp(argv[i],"-load") == 0) // flag "-load filename.xml"
+        {
+            i++;
+            filename_to_load = argv[i];
+        }
+    }
+
+/*
+ * train a model
+ * params: 4 layer 100 100 fully connected
+ * method_param = 0.001, epoch=100
+ * */
+//    build_mlp_classifier( data_filename, filename_to_save, filename_to_load );
+
+
+    Ptr<ANN_MLP> model;
+    model = load_classifier<ANN_MLP>("./res/Glass_ann_model.xml");
+    float CE[]  = {
+            2,1.51708,13.72,3.68,1.81,72.06,0.64,7.88,0,0
+//            7,1.51711,14.23,0,2.08,73.36,0,8.62,1.67,0
+
+    };
+    Mat sample1 = (Mat_<float>(1,9) << CE[1],CE[2],CE[3],CE[4],CE[5],CE[6],CE[7],CE[8],CE[9]);
+//    Mat sample1 = (Mat_<float>(1,10) << 1.51711,14.23,0,2.08,73.36,0,8.62,1.67,0,0);
+    cout << "sample1:"<<sample1<< endl;
+    Mat response_mat;
+    float r = model->predict( sample1 );
+    cout << "predict result :"<< r <<endl;
+
+
+//    waitKey(0);
 
     return 0;
 }
